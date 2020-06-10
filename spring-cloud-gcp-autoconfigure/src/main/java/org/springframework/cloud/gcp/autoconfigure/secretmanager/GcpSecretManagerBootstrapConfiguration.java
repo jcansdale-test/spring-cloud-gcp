5fa50f7a36cc031210e5e41c23492105a520f47d
Travis Tomsu <ttomsu@google.com>
2020-06-10 15:12:11 -0400

    tests: Removes various unnecessary network calls

--- spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/secretmanager/GcpSecretManagerBootstrapConfiguration.java
+++ spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/secretmanager/GcpSecretManagerBootstrapConfiguration.java
@@ -28,7 +28,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
-import org.springframework.cloud.gcp.core.DefaultCredentialsProvider;
 import org.springframework.cloud.gcp.core.DefaultGcpProjectIdProvider;
 import org.springframework.cloud.gcp.core.GcpProjectIdProvider;
 import org.springframework.cloud.gcp.core.UserAgentHeaderProvider;
@@ -59,9 +58,10 @@ public class GcpSecretManagerBootstrapConfiguration {
 
 	public GcpSecretManagerBootstrapConfiguration(
 			GcpSecretManagerProperties properties,
+			CredentialsProvider credentialsProvider,
 			ConfigurableEnvironment configurableEnvironment) throws IOException {
 
-		this.credentialsProvider = new DefaultCredentialsProvider(properties);
+		this.credentialsProvider = credentialsProvider;
 		this.gcpProjectIdProvider = properties.getProjectId() != null
 				? properties::getProjectId
 				: new DefaultGcpProjectIdProvider();
